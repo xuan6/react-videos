69e562909b9ce881ac775637c876bb5f59c57b12
Xuan liu <xuanliu@uw.edu>
2017-05-27 10:49:48 -0700

    update searchbox

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,11 +5,18 @@ class SearchBar extends Component {
 		super(props);
 		this.state = {term: ''};
 	}
+
+	onInputChange(term){
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
 	
 	render () {
 		return (
-		<div>
-		<input onChange = {event => this.setState({term:event.target.value})} />
+		<div className="search-bar">
+		<input 
+			value={this.state.term}
+			onChange = {event => this.onInputChange(event.target.value)} />
 		</div>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -12,24 +13,29 @@ class APP extends Component {
     this.state = {
       videos:[],
       selectedVideo: null
-    }
+    };
 
-    YTSearch({key:API_KEY, term:'surfboard'},(videos) => {
+    this.videoSearch('surfboard');
+  }
+
+  videoSearch(term){
+        YTSearch({key:API_KEY, term:term},(videos) => {
       this.setState({
         videos:videos,
         selectedVideo:videos[0]
       })
     });
-
   }
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+    
     return (
     <div>
-    <SearchBar />
-    <VideoDetail video={this.state.selectedVideo} />
-    <VideoList
-    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-    videos={this.state.videos}/>
+      <SearchBar onSearchTermChange={videoSearch}/>
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+      videos={this.state.videos}/>
     </div>
   );
   }  
