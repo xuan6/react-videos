ce64a872d01c2ea68cd50ad76bf5d4301f001896
Xuan liu <xuanliu@uw.edu>
2017-05-30 22:08:37 -0700

    update search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,7 +14,8 @@ class SearchBar extends Component {
 	render () {
 		return (
 		<div className="search-bar">
-		<input 
+		Search videos
+		<input
 			value={this.state.term}
 			onChange = {event => this.onInputChange(event.target.value)} />
 		</div>
